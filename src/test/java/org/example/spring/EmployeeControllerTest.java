2c135ac97c93f64c6cc5d6c03665055a1d6f1995
HUANGLI8 <lily.huang@oocl.com>
2025-09-09 17:18:43 +0800

    test 1 2

---
+++ src/test/java/org/example/spring/EmployeeControllerTest.java
@@ -0,0 +1,64 @@
+package org.example.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_create_employee_when_given_a_valid_body() throws Exception{
+        String requestBody = """
+        {
+           "name": "John",
+           "age":30,
+           "gender":"MALE",
+           "salary":5000
+        }
+        """
+              ;
+        mockMvc.perform(post("/employees1").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("John"))
+                .andExpect(jsonPath("$.age").value(30))
+                .andExpect(jsonPath("$.gender").value("MALE"))
+                .andExpect(jsonPath("$.salary").value(5000));
+
+    }
+
+    @Test
+    void should_get_employee_when_given_a_valid_id() throws Exception{
+        String requestBody = """
+        {
+           "name": "John",
+           "age":30,
+           "gender":"MALE",
+           "salary":5000
+        }
+        """
+                ;
+        mockMvc.perform(post("/employees1").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isCreated());
+        mockMvc.perform(get("/employees/{id}",1).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("John"))
+                .andExpect(jsonPath("$.age").value(30))
+                .andExpect(jsonPath("$.gender").value("MALE"))
+                .andExpect(jsonPath("$.salary").value(5000));
+    }
+
+    @Test
+}
